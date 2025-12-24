2dc4370c1584205eae62f060cb40bf4a0f67b3bc
AliceFantasia <16798.smr@gmail.com>
2025-12-24 15:42:17 +0700

    Added solution for exercise 1.1-1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,51 @@
+  const Header = (props) => {
+    return <h1>{props.name}</h1>;
+  }
+
+  const Part = (props) => {
+    return <p> {props.part} {props.exercises}</p>
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part part={props.parts[0].name} exercises={props.parts[0].number}/>
+        <Part part={props.parts[1].name} exercises={props.parts[1].number}/>
+        <Part part={props.parts[2].name} exercises={props.parts[2].number}/>
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return <p>Number of exercises: {props.parts[0].number + props.parts[1].number + props.parts[2].number}</p>
+  }
+
+const App = () => {
+  const Course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        number: 10
+      },
+      {
+        name: 'Using props to pass data',
+        number: 7
+      },
+      {
+        name: 'State of a component',
+        number: 14
+      },
+    ]
+  }
+
+  return (
+    <div>
+      <Header name={Course.name}/>
+      <Content parts={Course.parts}/>
+      <Total parts={Course.parts}/>
+    </div>
+  )
+}
+
+export default App
